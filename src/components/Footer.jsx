5f49be1b024cd1bb35e1fdd6581ddc20d5faa6a8
vladian95 <kvankvan1995@gmail.com>
2023-02-21 14:36:57 +0300

    added footer and home page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,16 @@
-import { Facebook, Instagram, Pinterest, Twitter } from '@mui/icons-material';
+import {
+  Facebook,
+  Instagram,
+  Mail,
+  Phone,
+  Pinterest,
+  Place,
+  Twitter,
+} from '@mui/icons-material';
 import styled from 'styled-components';
 
 const Container = styled.div`
-    display: flex:
+  display: flex;
 `;
 
 const Left = styled.div`
@@ -33,9 +41,37 @@ const IconContainer = styled.div`
 
 const Center = styled.div`
   flex: 1;
+  padding: 20px;
 `;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
 const Right = styled.div`
   flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-item: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
 `;
 
 const Footer = () => {
@@ -64,8 +100,36 @@ const Footer = () => {
           </IconContainer>
         </SocialContainer>
       </Left>
-      <Center></Center>
-      <Right></Right>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Place style={{ marginRight: '10px' }} /> 622 Dixie Path , South
+          982262
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: '10px' }} />
+          +7 978 123 45 67
+        </ContactItem>
+        <ContactItem>
+          <Mail style={{ marginRight: '10px' }} /> kvankvan1995@gmail.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
     </Container>
   );
 };
